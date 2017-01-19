515be3873b5fe0a1043a9cd89af3c051813cc3d7
Syaiful Bahri <syaiful@pengunjungblog.com>
2017-01-19 07:04:31 +0700

    mvar

--- frontend/src/Control/Monad/Aff/MVar.js
+++ frontend/src/Control/Monad/Aff/MVar.js
@@ -1,79 +1,87 @@
 "use strict";
 
 // Content
-function Blocked (p, c) {
-  this._p = p;
-  this._c = c;
-}
-
 function Empty () {}
 
 function Full (v) {
-  this._v = v;
+  this.val = v;
 }
 
-function Killed(e) {
-  this._e = e;
+function Killed(err) {
+  this.err = err;
+}
+
+function Consumers(blocked) {
+  this.blocked = blocked;
+}
+
+function Producers(blocked) {
+  this.blocked = blocked;
 }
 
 exports._makeMVar = function (nonCanceler) {
   return function (success) {
     return success({ state: new Empty() });
     return nonCanceler;
-  }
+  };
 }
 
 exports._takeMVar = function (nonCanceler, mvar) {
   return function (success, error) {
     if (mvar.state instanceof Killed) {
-      error(mvar.state._e);
+      error(mvar.state.err);
     } else if (mvar.state instanceof Full) {
+      var val = mvar.state.val;
       mvar.state = new Empty();
-      success(mvar.state._v);
+      success(val);
     } else if (mvar.state instanceof Empty) {
-      mvar.state = new Blocked([], [{ success: success, error: error }]);
-    } else if (mvar.state instanceof Blocked) {
-      if (mvar.state._p.length > 0) {
-        mvar.state._p.shift()(success, error);
-        if (mvar.state._p.length === 0 && mvar.state._c.length === 0) {
-          mvar.state = new Empty();
-        }
-      } else {
-        mvar._c.push({ success: success, error: error });
+      mvar.state = new Consumers([{success: success, error: error }]);
+    } else if (mvar.state instanceof Consumers) {
+      mvar.state.blocked.push({ success: success, error: error });
+    } else if (mvar.state instanceof Producers) {
+      mvar.state.blocked.shift()(success, error);
+      if (mvar.state.blocked.length === 0) {
+        mvar.state = new Empty();
       }
     }
     return nonCanceler;
-  }
+  };
 }
 
 exports._putMVar = function (nonCanceler, mvar, a) {
   return function (success, error) {
     if (mvar.state instanceof Killed) {
-      error(mvar.state._e);
+      error(mvar.state.err);
     } else if (mvar.state instanceof Empty) {
       mvar.state = new Full(a);
-      success({});
+      success();
     } else if (mvar.state instanceof Full) {
-      mvar.state = new Blocked([
+      var val = mvar.state.val;
+      var ns = new Producers([
         function (s) {
-          s(mvar.state._v);
+          s(val);
           return nonCanceler;
         },
         function (s) {
           s(a);
+          // so it block until consumed
+          success();
           return nonCanceler;
         }
-      ], []);
-      success({});
-    } else if (mvar.state instanceof Blocked) {
-      for (var i = 0, len = mvar.state._c.length; i < len; i++) {
-        mvar.state._c[i].success(a);
-      }
-      mvar.state._c = [];
-      if (mvar.state._p.length === 0 && mvar.state._c.length === 0) {
+      ]);
+      mvar.state = ns;
+    } else if (mvar.state instanceof Consumers) {
+      var consumers = mvar.state.blocked.shift();
+      consumers.success(a);
+      if (mvar.state.blocked.length === 0) {
         mvar.state = new Empty();
       }
-      success({});
+      success();
+    } else if (mvar.state instanceof Producers) {
+      mvar.state.blocked.push(function (s) {
+        s(a);
+        success();
+      });
     }
     return nonCanceler;
   };
@@ -82,9 +90,9 @@ exports._putMVar = function (nonCanceler, mvar, a) {
 exports._killMVar = function (nonCanceler, mvar, e) {
   return function (success, error) {
     if (mvar.state instanceof Killed) {
-      error(mvar.state._e);
-    } else if (mvar.state instanceof Blocked) {
-      var consumers = mvar.state._c;
+      error(mvar.state.err);
+    } else if (mvar.state instanceof Consumers) {
+      var consumers = mvar.state.blocked;
       while (consumers.length) {
         consumers.shift().error(e);
       }
--- frontend/test/MVar.purs
+++ frontend/test/MVar.purs
@@ -9,8 +9,11 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Error.Class (throwError)
+import Control.Monad.Rec.Class (forever)
 
 import Data.Either (either)
+import Data.Array ((..))
+import Data.Foldable (traverse_)
 
 type TestMVar a = forall e. Aff (console :: CONSOLE, mvar :: MVAR | e) a
 
@@ -28,7 +31,30 @@ test_killMVar = do
   v <- makeMVar
   killMVar v (error "DOA")
   e <- attempt $ takeMVar v
-  either (const $ liftEff $ log "Success: Killed MVar") (const $ liftEff $ log "Failure: Oh noes, MVar survived!") e
+  either
+    (const $ liftEff $ log "Success: Killed MVar")
+    (const $ liftEff $ log "Failure: Oh noes, MVar survived!")
+    e
+
+test_roundRobin :: TestMVar Unit
+test_roundRobin = do
+  v <- makeMVar
+  forkAff $ forever $ do
+    val <- takeMVar v
+    liftEff $ log ("received value (Thread #1): " <> show val)
+  forkAff $ forever $ do
+    val <- takeMVar v
+    liftEff $ log ("received value (Thread #2): " <> show val)
+  traverse_ (putMVar v) (0..10)
+
+test_roundRobinPut :: TestMVar Unit
+test_roundRobinPut = do
+  v <- makeMVar
+  forkAff $ traverse_ (putMVar v) (0..10)
+  forkAff $ traverse_ (putMVar v) (11..20)
+  void $ forkAff $ forever $ do
+    val <- takeMVar v
+    liftEff $ log ("received: " <> show val)
 
 main :: forall e. Eff (console :: CONSOLE, mvar :: MVAR, err :: EXCEPTION | e) Unit
 main =
@@ -38,3 +64,9 @@ main =
 
     liftEff $ log "Testing kill var"
     test_killMVar
+
+    liftEff $ log "Testing round robin"
+    test_roundRobin
+
+    liftEff $ log "Testing put block until it consumed"
+    test_roundRobinPut
